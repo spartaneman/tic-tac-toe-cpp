077dcb818238a0baf3386ce2395c965368280b05
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-07-27 18:34:01 -0700

    Initial set up and psuedocode

---
+++ tic-tac-toe.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+/*Author: Emanuel Ruiz
+ *Start Date: 7/27/23
+ *Version 1.0
+ *
+ *Description: 
+ *Simple tic tac toe game that will take the input of two players. 
+ *The players will be asked to decide the size of the map which will then be displayed to them
+ */
+
+/**Psuedo code
+ * Create game function that will loop as long as the users wishes to play the game. 
+ *      ask user for size of map. 
+ *      create map.
+ * Create function that displays map
+ * Create function that updates player move.
+ *      checks whether move is valid 
+ * Create function that checks whether a player has won, 
+ *      checks whether there aren't any more possible spaces available
+*/
+int main(){
+    return 0;
+}
